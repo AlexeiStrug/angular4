9720de63cc24e048e6b9708ed35dfb3c9ff1d491
DESKTOP-SPAH5K1\Alex <alexeistrug@gmail.com>
2017-09-21 20:36:29 +0300

    initial new branches

--- systemjs.config.js
+++ systemjs.config.js
@@ -21,6 +21,7 @@
             'rxjs': 'npm:rxjs',
             'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
         },
+
         // пакеты, которые указывают загрузчику System, как загружать файлы без имени и расширения
         packages: {
             app: {
